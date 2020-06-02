a36f23e82520f55ff5c89de8ebc1f3fd9ef464f8
Vimal Krishnan <akvimal@gmail.com>
2020-06-02 19:59:18 +0530

    integrated with react

---
+++ build/precache-manifest.1b4540c6190e66847b6780f9beb15aeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "054e58ca763da77f48e020b0fff9f27a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b843489b01e7296752f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67f3e933b3ad2a9f299b",
+    "url": "/static/js/2.74cebf35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.74cebf35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b843489b01e7296752f",
+    "url": "/static/js/main.b23e9665.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
